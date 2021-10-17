a845e2b1f465f07c1809bd69c152de213786cf8d
emanuele-progr <marioemanuele.ghianni@stud.unifi.it>
2021-10-17 20:17:57 +0200

    Add files via upload

---
+++ data_utils.py
@@ -0,0 +1,286 @@
+import numpy as np
+import torch
+import torchvision
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import random_split, ConcatDataset
+from sklearn.model_selection import train_test_split
+import torchvision.transforms.functional as TorchVisionFunc
+
+
+def get_permuted_mnist(task_id, batch_size):
+	"""
+	Get the dataset loaders (train and test) for a `single` task of permuted MNIST.
+	This function will be called several times for each task.
+	
+	:param task_id: id of the task [starts from 1]
+	:param batch_size:
+	:return: a tuple: (train loader, test loader)
+	"""
+	
+	# convention, the first task will be the original MNIST images, and hence no permutation
+	if task_id == 1:
+		idx_permute = np.array(range(784))
+	else:
+		idx_permute = torch.from_numpy(np.random.RandomState().permutation(784))
+	transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+				torchvision.transforms.Lambda(lambda x: x.view(-1)[idx_permute] ),
+				])
+	mnist_train = torchvision.datasets.MNIST('./data/', train=True, download=True, transform=transforms)
+	train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, num_workers=4, pin_memory=True, shuffle=True)
+	test_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('./data/', train=False, download=True, transform=transforms),  batch_size=256, shuffle=False, num_workers=4, pin_memory=True)
+
+	return train_loader, test_loader
+
+
+def get_permuted_mnist_tasks(num_tasks, batch_size):
+	"""
+	Returns the datasets for sequential tasks of permuted MNIST
+	
+	:param num_tasks: number of tasks.
+	:param batch_size: batch-size for loaders.
+	:return: a dictionary where each key is a dictionary itself with train, and test loaders.
+	"""
+	datasets = {}
+	for task_id in range(1, num_tasks+1):
+		train_loader, test_loader = get_permuted_mnist(task_id, batch_size)
+		datasets[task_id] = {'train': train_loader, 'test': test_loader}
+	return datasets
+
+
+class RotationTransform:
+	"""
+	Rotation transforms for the images in `Rotation MNIST` dataset.
+	"""
+	def __init__(self, angle):
+		self.angle = angle
+
+	def __call__(self, x):
+		return TorchVisionFunc.rotate(x, self.angle, fill=(0,))
+
+
+def get_rotated_mnist(task_id, batch_size):
+	"""
+	Returns the dataset for a single task of Rotation MNIST dataset
+	:param task_id:
+	:param batch_size:
+	:return:
+	"""
+	per_task_rotation = 10
+	rotation_degree = (task_id - 1)*per_task_rotation
+	rotation_degree -= (np.random.random()*per_task_rotation)
+
+	transforms = torchvision.transforms.Compose([
+		RotationTransform(rotation_degree),
+		torchvision.transforms.ToTensor(),
+		])
+
+	train_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('./data/', train=True, download=True, transform=transforms), batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+	test_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('./data/', train=False, download=True, transform=transforms),  batch_size=256, shuffle=False, num_workers=4, pin_memory=True)
+
+	return train_loader, test_loader
+
+
+def get_rotated_mnist_tasks(num_tasks, batch_size):
+	"""
+	Returns data loaders for all tasks of rotation MNIST dataset.
+	:param num_tasks: number of tasks in the benchmark.
+	:param batch_size:
+	:return:
+	"""
+	datasets = {}
+	for task_id in range(1, num_tasks+1):
+		train_loader, test_loader = get_rotated_mnist(task_id, batch_size)
+		datasets[task_id] = {'train': train_loader, 'test': test_loader}
+	return datasets
+
+
+def get_split_cifar100(task_id, batch_size, cifar_train, cifar_test):
+	"""
+	Returns a single task of split CIFAR-100 dataset
+	:param task_id:
+	:param batch_size:
+	:return:
+	"""
+	
+
+	start_class = (task_id-1)*5
+	end_class = task_id * 5
+
+	targets_train = torch.tensor(cifar_train.targets)
+	target_train_idx = ((targets_train >= start_class) & (targets_train < end_class))
+	
+	targets_test = torch.tensor(cifar_test.targets)
+	target_test_idx = ((targets_test >= start_class) & (targets_test < end_class))
+
+	train_loader = torch.utils.data.DataLoader(torch.utils.data.dataset.Subset(cifar_train, np.where(target_train_idx==1)[0]), batch_size=batch_size, shuffle=True)
+	test_loader = torch.utils.data.DataLoader(torch.utils.data.dataset.Subset(cifar_test, np.where(target_test_idx==1)[0]), batch_size=batch_size)
+
+	return train_loader, test_loader
+
+
+def get_split_cifar100_tasks(num_tasks, batch_size):
+	"""
+	Returns data loaders for all tasks of split CIFAR-100
+	:param num_tasks:
+	:param batch_size:
+	:return:
+	"""
+	datasets = {}
+	
+	# convention: tasks starts from 1 not 0 !
+	# task_id = 1 (i.e., first task) => start_class = 0, end_class = 4
+	cifar_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),])
+	cifar_train = torchvision.datasets.CIFAR100('./data/', train=True, download=True, transform=cifar_transforms)
+	cifar_test = torchvision.datasets.CIFAR100('./data/', train=False, download=True, transform=cifar_transforms)
+	
+	for task_id in range(1, num_tasks+1):
+		train_loader, test_loader = get_split_cifar100(task_id, batch_size, cifar_train, cifar_test)
+		datasets[task_id] = {'train': train_loader, 'test': test_loader}
+	return datasets
+
+# if __name__ == "__main__":
+# 	dataset = get_split_cifar100(1)
+
+def get_split_cifar100_tasks2(num_tasks, batch_size):
+
+	datasets = {}
+
+	# convention: tasks starts from 1 not 0 !
+	# task_id = 1 (i.e., first task) => start_class = 0, end_class = 4
+	cifar_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),])
+	cifar_train = torchvision.datasets.CIFAR100('./data/', train=True, download=True, transform=cifar_transforms)
+	cifar_test = torchvision.datasets.CIFAR100('./data/', train=False, download=True, transform=cifar_transforms)
+
+	num_elements_train = len(cifar_train)/num_tasks
+	num_elements_test = len(cifar_test)/2
+
+
+	#test_indices, _ = train_test_split(list(range(len(cifar_test.targets))), train_size = num_elements_test, stratify = cifar_test.targets)
+	#test_dataset = torch.utils.data.Subset(cifar_test, test_indices)
+
+	test_ds, val_ds = random_split(cifar_test, [int(num_elements_test), int(num_elements_test)], generator=torch.Generator().manual_seed(42))
+
+
+	test_loader = torch.utils.data.DataLoader(test_ds, batch_size=batch_size)
+	val_loader  = torch.utils.data.DataLoader(val_ds, batch_size=batch_size)
+
+	list_item = list(range(len(cifar_train.targets)))
+
+	train = cifar_train
+
+
+	
+	for task_id in range(1, num_tasks+1):
+
+		train_ds, residual = random_split(train, [int(num_elements_train), int((len(train)-num_elements_train))], generator=torch.Generator().manual_seed(42))
+		train_loader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
+		train = residual
+		'''
+		train_indices, cifar_train_indices = train_test_split(list_item, train_size = num_elements_train, stratify = cifar_train.targets)
+		train_dataset = torch.utils.data.Subset(cifar_train, train_indices)
+		train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
+		list_item = list(set(list_item) - set(cifar_train_indices))
+		'''
+		datasets[task_id] = {'train': train_loader, 'test': test_loader, 'val': val_loader}
+		
+	return datasets
+
+def get_split_cifar100_tasks_joint(num_tasks, batch_size):
+
+
+    datasets = {}
+
+    # convention: tasks starts from 1 not 0 !
+    # task_id = 1 (i.e., first task) => start_class = 0, end_class = 4
+    cifar_train_transforms = torchvision.transforms.Compose(
+        [torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.RandomCrop(32,padding=4,padding_mode="reflect"),torchvision.transforms.ToTensor(),])
+    cifar_test_transforms = torchvision.transforms.Compose(
+        [torchvision.transforms.ToTensor(),])
+    cifar_train = torchvision.datasets.CIFAR100(
+        './data/', train=True, download=True, transform=cifar_train_transforms)
+    cifar_test = torchvision.datasets.CIFAR100(
+        './data/', train=False, download=True, transform=cifar_test_transforms)
+
+    num_elements_train = len(cifar_train)/num_tasks
+    num_elements_test = len(cifar_test)/2
+    
+    test_ds, val_ds = random_split(cifar_test, [int(num_elements_test), int(num_elements_test)], generator=torch.Generator().manual_seed(42))
+
+    # test_indices, _ = train_test_split(list(range(len(cifar_test.targets))), train_size = num_elements_test, stratify = cifar_test.targets)
+    # test_dataset = torch.utils.data.Subset(cifar_test, test_indices)
+
+    test_loader = torch.utils.data.DataLoader(test_ds, batch_size=batch_size)
+    val_loader = torch.utils.data.DataLoader(val_ds, batch_size=batch_size)
+
+    list_item = list(range(len(cifar_train.targets)))
+
+    train = cifar_train
+
+    for task_id in range(1, num_tasks+1):
+
+        train_ds, residual = random_split(train, [int(num_elements_train), int(
+            (len(train)-num_elements_train))], generator=torch.Generator().manual_seed(42))
+        if task_id == 1:
+            train_j = train_ds
+        else:
+            train_j = torch.utils.data.ConcatDataset([train_j, train_ds])
+        train_loader = torch.utils.data.DataLoader(train_j, batch_size=batch_size)
+        train = residual
+        '''
+        train_indices, cifar_train_indices = train_test_split(
+            list_item, train_size = num_elements_train, stratify = cifar_train.targets)
+        train_dataset = torch.utils.data.Subset(cifar_train, train_indices)
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=batch_size)
+        list_item = list(set(list_item) - set(cifar_train_indices))
+        '''
+        datasets[task_id] = {'train': train_loader, 'test': test_loader, 'val': val_loader}
+
+    return datasets
+
+def get_split_cifar10_tasks(num_tasks, batch_size):
+
+	datasets = {}
+	
+	# convention: tasks starts from 1 not 0 !
+	# task_id = 1 (i.e., first task) => start_class = 0, end_class = 4
+	cifar_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),])
+	cifar_train = torchvision.datasets.CIFAR10('./data/', train=True, download=True, transform=cifar_transforms)
+	cifar_test = torchvision.datasets.CIFAR10('./data/', train=False, download=True, transform=cifar_transforms)
+
+	num_elements_train = len(cifar_train)/num_tasks
+	num_elements_test = len(cifar_test)/2
+
+
+	#test_indices, _ = train_test_split(list(range(len(cifar_test.targets))), train_size = num_elements_test, stratify = cifar_test.targets)
+	#test_dataset = torch.utils.data.Subset(cifar_test, test_indices)
+
+
+	
+	test_ds, val_ds = random_split(cifar_test, [int(num_elements_test), int(num_elements_test)], generator=torch.Generator().manual_seed(42))
+
+
+	test_loader = torch.utils.data.DataLoader(test_ds, batch_size=batch_size)
+	val_loader  = torch.utils.data.DataLoader(val_ds, batch_size=batch_size)
+
+	list_item = list(range(len(cifar_train.targets)))
+
+	train = cifar_train
+
+
+	
+	for task_id in range(1, num_tasks+1):
+
+		train_ds, residual = random_split(train, [int(num_elements_train), int((len(train)-num_elements_train))], generator=torch.Generator().manual_seed(42))
+		train_loader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
+		train = residual
+		'''
+		train_indices, cifar_train_indices = train_test_split(list_item, train_size = num_elements_train, stratify = cifar_train.targets)
+		train_dataset = torch.utils.data.Subset(cifar_train, train_indices)
+		train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
+		list_item = list(set(list_item) - set(cifar_train_indices))
+		'''
+		datasets[task_id] = {'train': train_loader, 'test': test_loader, 'val': val_loader}
+		
+	return datasets
---
+++ main.py
@@ -0,0 +1,358 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch.nn as nn
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.figure as figure
+from model import MLP, ResNet18, ResNet32
+from data_utils import get_permuted_mnist_tasks, get_rotated_mnist_tasks, get_split_cifar100_tasks2, get_split_cifar100_tasks, get_split_cifar10_tasks, get_split_cifar100_tasks_joint
+from utils import parse_arguments, DEVICE, init_experiment, end_experiment, log_metrics, save_checkpoint
+from sklearn.metrics import confusion_matrix
+
+
+def train_single_epoch(net, optimizer, loader, criterion, task_id=None):
+	"""
+	Train the model for a single epoch
+	
+	:param net:
+	:param optimizer:
+	:param loader:
+	:param criterion:
+	:param task_id:
+	:return:
+	"""
+	net = net.to(DEVICE)
+	net.train()
+	
+	for batch_idx, (data, target) in enumerate(loader):
+		data = data.to(DEVICE)
+		target = target.to(DEVICE)
+		optimizer.zero_grad()
+		if task_id:
+			pred = net(data, task_id)
+		else:
+			pred = net(data)
+		loss = criterion(pred, target)
+		loss.backward()
+		optimizer.step()
+	return net
+
+
+def eval_single_epoch(net, loader, criterion, task_id=None):
+	"""
+	Evaluate the model for single epoch
+	
+	:param net:
+	:param loader:
+	:param criterion:
+	:param task_id:
+	:return:
+	"""
+	net = net.to(DEVICE)
+	net.eval()
+	test_loss = 0
+	correct = 0
+	with torch.no_grad():
+		for data, target in loader:
+			data = data.to(DEVICE)
+			target = target.to(DEVICE)
+			# for cifar head
+			if task_id is not None:
+				output = net(data, task_id)
+			else:
+				output = net(data)
+			test_loss += criterion(output, target).item()
+			pred = output.data.max(1, keepdim=True)[1]
+			correct += pred.eq(target.data.view_as(pred)).sum()
+	test_loss /= len(loader.dataset)
+	correct = correct.to('cpu')
+	avg_acc = 100.0 * float(correct.numpy()) / len(loader.dataset)
+	return {'accuracy': avg_acc, 'loss': test_loss}
+
+
+def final_eval(net, loader, criterion, task_id=None):
+    """
+    Evaluate the model for single epoch
+    
+    :param net:
+    :param loader:
+    :param criterion:
+    :param task_id:
+    :return:
+    """
+    X = []
+    Y = []
+    net = net.to(DEVICE)
+    net.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in loader:
+            data = data.to(DEVICE)
+            target = target.to(DEVICE)
+            # for cifar head
+            if task_id is not None:
+                output = net(data, task_id)
+            else:
+                output = net(data)
+            test_loss += criterion(output, target).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            Y.append(pred.view_as(target.data).cpu().numpy().tolist())
+            X.append(target.data.cpu().numpy().tolist())
+            correct += pred.eq(target.data.view_as(pred)).sum()
+    test_loss /= len(loader.dataset)
+    correct = correct.to('cpu')
+    avg_acc = 100.0 * float(correct.numpy()) / len(loader.dataset)
+    X = sum(X, [])
+    Y = sum(Y, [])
+    return {'accuracy': avg_acc, 'loss': test_loss}, X, Y
+
+def get_benchmark_data_loader(args):
+	"""
+	Returns the benchmark loader which could be either of these:
+	get_split_cifar100_tasks, get_permuted_mnist_tasks, or get_rotated_mnist_tasks
+	
+	:param args:
+	:return: a function which when called, returns all tasks
+	"""
+	if args.dataset == 'perm-mnist' or args.dataset == 'permuted-mnist':
+		return get_permuted_mnist_tasks
+	elif args.dataset == 'rot-mnist' or args.dataset == 'rotation-mnist':
+		return get_rotated_mnist_tasks
+	elif args.dataset == 'cifar-100' or args.dataset == 'cifar100' and args.compute_joint_incremental:
+		return get_split_cifar100_tasks_joint
+	elif args.dataset == 'cifar-100' or args.dataset == 'cifar100' and args.compute_joint_incremental is False:
+		return get_split_cifar100_tasks2
+	elif args.dataset == 'cifar-10' or args.dataset == 'cifar10':
+		return get_split_cifar10_tasks
+	else:
+		raise Exception("Unknown dataset.\n"+
+						"The code supports 'perm-mnist, rot-mnist, and cifar-100.")
+
+
+def get_benchmark_model(args):
+	"""
+	Return the corresponding PyTorch model for experiment
+	:param args:
+	:return:
+	"""
+	if 'mnist' in args.dataset:
+		if args.tasks == 20 and args.hiddens < 256:
+			print("Warning! the main paper MLP with 256 neurons for experiment with 20 tasks")
+		return MLP(args.hiddens, {'dropout': args.dropout}).to(DEVICE)
+	elif 'cifar' in args.dataset:
+		return ResNet32(config={'dropout': args.dropout}).to(DEVICE)
+	else:
+		raise Exception("Unknown dataset.\n"+
+						"The code supports 'perm-mnist, rot-mnist, and cifar-100.")
+
+def plot_conf_matrix(matrix):
+
+    plt.figure(1, figsize=(9, 6))
+ 
+    plt.title("Confusion Matrix")
+    df_cm = pd.DataFrame(matrix, range(len(matrix[0])), range(len(matrix[0])))
+
+    sns.set(font_scale=1.4) # for label size
+    sns.heatmap(df_cm, annot=True, annot_kws={"size": 12},  fmt='g') # font size
+    s = "matrix"
+    print(s)
+    plt.savefig("confmatrix", bbox_inches='tight', dpi=300)
+    plt.show()
+    plt.close()
+
+
+def run(args):
+	"""
+	Run a single run of experiment.
+	
+	:param args: please see `utils.py` for arguments and options
+	"""
+	# init experiment
+	acc_db, loss_db, hessian_eig_db = init_experiment(args)
+
+	# load benchmarks and model
+	print("Loading {} tasks for {}".format(args.tasks, args.dataset))
+	tasks = get_benchmark_data_loader(args)(args.tasks, args.batch_size)
+	print("loaded all tasks!")
+	model = get_benchmark_model(args)
+
+	# criterion
+	criterion = nn.CrossEntropyLoss().to(DEVICE)
+	time = 0
+	
+	for current_task_id in range(1, args.tasks+1):
+		print("================== TASK {} / {} =================".format(current_task_id, args.tasks))
+		train_loader = tasks[current_task_id]['train']
+		lr = max(args.lr * args.gamma ** (current_task_id), 0.00005)
+		
+		for epoch in range(1, args.epochs_per_task+1):
+			# 1. train and save
+	
+			optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+			train_single_epoch(model, optimizer, train_loader, criterion, current_task_id)
+			time += 1
+
+			model = model.to(DEVICE)
+			val_loader = tasks[current_task_id]['val']
+			metrics = eval_single_epoch(model, val_loader, criterion, current_task_id)
+			acc_db, loss_db = log_metrics(metrics, time, current_task_id, acc_db, loss_db)
+			print(acc_db[current_task_id][epoch])
+			#if epoch == args.epochs_per_task:
+			#	tune.report(val_accuracy= acc_db[current_task_id][epoch])
+
+			# 2. evaluate on all tasks up to now, including the current task
+			for prev_task_id in range(1, current_task_id+1):
+				# 2.0. only evaluate once a task is finished
+				if epoch == args.epochs_per_task:
+					model = model.to(DEVICE)
+					val_loader = tasks[prev_task_id]['test']
+					
+					# 2.1. compute accuracy and loss
+					metrics = eval_single_epoch(model, val_loader, criterion, prev_task_id)
+					acc_db, loss_db = log_metrics(metrics, time, prev_task_id, acc_db, loss_db)
+					
+					# 2.2. (optional) compute eigenvalues and eigenvectors of Loss Hessian
+                    
+					#if prev_task_id == current_task_id and args.compute_eigenspectrum:
+					#	hessian_eig_db = log_hessian(model, val_loader, time, prev_task_id, hessian_eig_db)
+
+						
+					# 2.3. save model parameters
+					save_checkpoint(model, time)
+
+	end_experiment(args, acc_db, loss_db, hessian_eig_db)
+
+
+
+def run_experiment(args):
+
+
+	tasks = get_benchmark_data_loader(args)(args.tasks, args.batch_size)
+	model = get_benchmark_model(args)
+	acc_db, loss_db, hessian_eig_db = init_experiment(args)
+
+
+	# criterion
+	criterion = nn.CrossEntropyLoss().to(DEVICE)
+	time = 0
+	the_last_loss = 100
+	patience = 30
+	trigger_times = 0
+	check = 0
+	#lr = [0.01, 0.001, 0.001, 0.001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001]
+	lr = [0.001, 0.0001, 0.0001, 0.0001, 0.0001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001]
+	#lr = [0.001, 0.0001, 0.0001, 0.00001, 0.00001]
+
+
+	for current_task_id in range(1, args.tasks+1):
+		#lr = max(args.lr * args.gamma ** (current_task_id), 0.00005)
+		if args.compute_joint_incremental:
+			model = get_benchmark_model(args)
+		print("================== TASK {} / {} =================".format(current_task_id, args.tasks))
+		train_loader = tasks[current_task_id]['train']
+		optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)#lr=lr[current_task_id - 1])
+		if (check == 1) :
+			model, optimizer = load_checkpoint(model, optimizer, 'check.pth')
+		
+		
+		for epoch in range(1, args.epochs_per_task+1):
+			# 1. train and save
+
+			prev_model = model
+			prev_opt = optimizer
+			train_single_epoch(model, optimizer, train_loader, criterion, current_task_id)
+			time += 1
+			model = model.to(DEVICE)
+			val_loader = tasks[current_task_id]['val']
+			metrics = eval_single_epoch(model, val_loader, criterion, current_task_id)
+			acc_db, loss_db = log_metrics(metrics, time, current_task_id, acc_db, loss_db)
+			
+
+			if loss_db[current_task_id][epoch-1] > the_last_loss:
+				if trigger_times == 0:
+					backup_model = prev_model
+					backup_opt = prev_opt
+				trigger_times += 1
+				print('trigger times:', trigger_times)
+			else:
+				trigger_times = 0
+			if trigger_times >= patience:
+				print('Early stopping!')
+				#tune.report(val_loss = loss_db[current_task_id][epoch])
+				model = backup_model.to(DEVICE)
+				val_loader = tasks[current_task_id]['test']
+				
+				# 2.1. compute accuracy and loss
+				metrics, X, Y = final_eval(model, val_loader, criterion, current_task_id)
+				matrix = confusion_matrix(X, Y)
+				#plot_conf_matrix(matrix)
+				acc_db, loss_db = log_metrics(metrics, time, current_task_id, acc_db, loss_db)
+				save_checkpoint_Adam(backup_model, backup_opt)
+				time = 0
+				trigger_times = 0
+				the_last_loss = 100
+				print(X)
+				print(Y)
+				break
+
+
+			if loss_db[current_task_id][epoch-1] < the_last_loss:
+				the_last_loss = loss_db[current_task_id][epoch-1]
+				
+			if epoch == args.epochs_per_task:
+				#tune.report(val_loss= loss_db[current_task_id][epoch])
+				if trigger_times > 0:
+					model = backup_model.to(DEVICE)
+					s = "ok"
+					print(s)
+				else:
+					model = model.to(DEVICE)
+				val_loader = tasks[current_task_id]['test']
+					
+				# 2.1. compute accuracy and loss
+				metrics, X, Y = final_eval(model, val_loader, criterion, current_task_id)
+				matrix = confusion_matrix(X, Y)
+				#plot_conf_matrix(matrix)
+				acc_db, loss_db = log_metrics(metrics, time, current_task_id, acc_db, loss_db)
+				save_checkpoint_Adam(model, optimizer)
+				print(X)
+				print(Y)
+				time = 0
+				trigger_times = 0
+				the_last_loss = 100
+	return
+    
+def load_checkpoint(model, optimizer, filename='check.pth'):
+    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
+    start_epoch = 0
+    if os.path.isfile(filename):
+        print("=> loading checkpoint '{}'".format(filename))
+        checkpoint = torch.load(filename)
+        #start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        #history = checkpoint['history']
+        print("=> loaded checkpoint '{}'")
+    else:
+        print("=> no checkpoint found at '{}'".format(filename))
+
+    return model, optimizer
+    
+def save_checkpoint_Adam(model, optimizer):
+    PATH = './check.pth'
+    state = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict(),}
+    torch.save(state, PATH)
+    
+if __name__ == "__main__":
+    args = parse_arguments()
+    run_experiment(args)
+    #analysis = tune.run(tuning, config={"lr" : tune.grid_search([0.001, 0.01, 0.0001])})
+    #print("Best config: ", analysis.get_best_config(metric="val_loss"))
+    #run(args)
+
+
+	
